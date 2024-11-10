ca24358ed19f6c372ca6d33b42de2a544d76d509
moniquejohnsonn <101659813+moniquejohnsonn@users.noreply.github.com>
2024-11-10 13:16:28 -0500

    base files with loadingScreen and homeView + firebase packages

---
+++ SwipeShare/loadingScreen.swift
@@ -0,0 +1,18 @@
+//
+//  loadingScreen.swift
+//  SwipeShare
+//
+//  Created by Monique Johnson on 11/10/24.
+//
+
+import SwiftUI
+
+struct loadingScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    loadingScreen()
+}
