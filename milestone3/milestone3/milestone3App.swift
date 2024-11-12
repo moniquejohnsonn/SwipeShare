5d75ec1325115ae523b873e58671a7e3a08467b9
natattack03 <nb3044@columbia.edu>
2024-11-11 20:06:36 -0500

    settings page complete

---
+++ milestone3/milestone3/milestone3App.swift
@@ -0,0 +1,17 @@
+//
+//  milestone3App.swift
+//  milestone3
+//
+//  Created by Natalie Bran on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct milestone3App: App {
+    var body: some Scene {
+        WindowGroup {
+            SettingsView()
+        }
+    }
+}
