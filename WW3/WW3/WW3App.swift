770feb2a966c77e1a88ea5abb8011df1f3ace298
Kalei Ragland <kalei.ragland@gmail.com>
2024-11-12 21:20:16 -0500

    created dining hall view

---
+++ WW3/WW3/WW3App.swift
@@ -0,0 +1,17 @@
+//
+//  WW3App.swift
+//  WW3
+//
+//  Created by Kalei Ragland on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WW3App: App {
+    var body: some Scene {
+        WindowGroup {
+            DiningHallView()
+        }
+    }
+}
